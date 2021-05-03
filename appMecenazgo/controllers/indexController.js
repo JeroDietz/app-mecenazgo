077f136a742e69f13919555ce84d0c46db7c860d
JeroDietz <jero.dietz@hotmail.com.ar>
2021-05-02 21:38:46 -0300

    adelantando

---
+++ appMecenazgo/controllers/indexController.js
@@ -0,0 +1,20 @@
+const indexController = {
+    home :
+        function(req,res){
+            res.render('index');
+        },
+
+    login : 
+        function(req,res){
+            res.render('login');
+        },
+    
+    register : function(req,res){
+        res.render('register');
+    },
+    createProyect: function(req,res){
+        res.render('createProyect');
+    }
+}
+
+module.exports = indexController;
--- appMecenazgo/routes/index.js
+++ appMecenazgo/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const indexController = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.home);
+router.get('/login', indexController.login);
+router.get('/register', indexController.register);
+router.get('/createProyect', indexController.createProyect);
 
 module.exports = router;
